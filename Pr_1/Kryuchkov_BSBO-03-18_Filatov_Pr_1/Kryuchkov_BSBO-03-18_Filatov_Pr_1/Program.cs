263d8e004dbc7e33475140529658a3bc9c7238e5
Tarveiz <misha28.95@mail.ru>
2021-06-19 20:45:06 +0300

    Com_2

---
+++ Pr_1/Kryuchkov_BSBO-03-18_Filatov_Pr_1/Kryuchkov_BSBO-03-18_Filatov_Pr_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Kryuchkov_BSBO_03_18_Filatov_Pr_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов: ");
+            int cols = int.Parse(Console.ReadLine());
+            int[,] arr = new int[rows, cols];
+
+            Random r = new Random(); // объявление переменной для генерации случ. чисел
+            Console.WriteLine();
+            for (int i = 0; i < rows; i++) // массив заполнения случ. числами
+            {
+                Console.Write(i + ")");
+                for (int j = 0; j < cols; j++)
+                {
+                    arr[i, j] = r.Next(2);
+                    Console.Write("{0,3}", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(String.Join(" ", arr));
+
+
+            int prevNum = -1;
+            for (int i = 0; i < rows; i++)
+            {
+                prevNum = -1;
+                for (int j = 0; j < cols; j++)
+                {
+                    if (cols == 1)
+                    {
+                        Console.WriteLine("Мало данных для выполнения задания. Пожалуйста увеличьте количество столбцов.");
+                        return;
+                    }
+                    if (prevNum != arr[i, j])
+                    {
+                        prevNum = arr[i, j];
+                    }
+                    else break;
+                    if (j == cols - 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
